9bc85c692a92966cc1dbedd4467b1b76c57a17c9
Evan Wilkerson <evanwilkerson@Evans-MacBook-Pro.local>
2023-07-23 12:30:57 -0500

    enhanced macos-wifi-stats.sh script

--- macos-wifi-stats.sh
+++ macos-wifi-stats.sh
@@ -1,26 +1,38 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # Function(s)
 get_wifi_stats() {
+    local wifi_stats
     wifi_stats=$(sudo /usr/libexec/airportd info)
-    ssid=$(echo "$wifi_stats" | grep -oE "SSID: '[^']+'" | awk -F "'" '{print $2}')
-    bssid=$(echo "$wifi_stats" | grep -oE "BSSID: [[:xdigit:]:]+" | awk '{print $2}')
-    channel=$(echo "$wifi_stats" | grep -oE "Channel: [^ ]+" | awk '{print $2}')
-    active_phy=$(echo "$wifi_stats" | grep -oE "Active PHY: [^ ]+" | awk '{print $3}')
-    tx_rate=$(echo "$wifi_stats" | grep -oE "Tx Rate: [0-9.]+" | awk '{print $3}')
-    mcs=$(echo "$wifi_stats" | grep -oE "MCS: [0-9]+" | awk '{print $2}')
-    nss=$(echo "$wifi_stats" | grep -oE "NSS: [0-9]+" | awk '{print $2}')
-    rssi=$(echo "$wifi_stats" | grep -oE "RSSI: -?[0-9]+" | awk '{print $2}')
-    noise=$(echo "$wifi_stats" | grep -oE 'Noise: -?[0-9]+' | awk '{print $2}')
+    ssid=$(awk -F "'" '/SSID:/ {print $2}' <<< "$wifi_stats")
+    bssid=$(awk '/BSSID:/ {print $2}' <<< "$wifi_stats")
+    channel=$(awk '/Channel:/ {print $2}' <<< "$wifi_stats")
+    active_phy=$(awk '/Active PHY:/ {print $3}' <<< "$wifi_stats")
+    tx_rate=$(awk '/Tx Rate:/ {print $3}' <<< "$wifi_stats")
+    mcs=$(awk '/MCS:/ {print $2}' <<< "$wifi_stats")
+    nss=$(awk '/NSS:/ {print $2}' <<< "$wifi_stats")
+    rssi=$(awk '/RSSI:/ {print $2}' <<< "$wifi_stats")
+    noise=$(awk '/Noise:/ {print $2}' <<< "$wifi_stats")
     snr=$(( rssi - noise ))
 }
 
+# Check if the required command exists
+if ! command -v /usr/libexec/airportd &> /dev/null; then
+    echo "Error: 'airportd' command not found. Make sure you are running macOS." >&2
+    exit 1
+fi
+
 # Main script
 filename="wifi-check-$(date +'%Y%m%d-%H%M%S').txt"
-echo "writing output to $(pwd)/$filename"
+echo "Writing output to $(pwd)/$filename"
 echo "==================================================== script started at $(date +'%Y-%m-%d %H:%M:%S') ====================================================" | tee -a "$filename"
+
 while true; do
     get_wifi_stats
-    echo "$(date +'%Y-%m-%d %H:%M:%S') | SSID: $ssid | BSSID: $bssid | Channel: $channel | Active PHY: $active_phy | Tx Rate: $tx_rate Mbps | MCS: $mcs | NSS: $nss | RSSI: $rssi dBm | Noise: $noise dBm | SNR: $snr" dB | tee -a "$filename"
+    if [ "$ssid" = "(null)" ]; then
+        echo "$(date +'%Y-%m-%d %H:%M:%S') | Not connected" | tee -a "$filename"
+    else
+        echo "$(date +'%Y-%m-%d %H:%M:%S') | SSID: $ssid | BSSID: $bssid | Channel: $channel | Active PHY: $active_phy | Tx Rate: $tx_rate Mbps | MCS: $mcs | NSS: $nss | RSSI: $rssi dBm | Noise: $noise dBm | SNR: $snr dB" | tee -a "$filename"
+    fi
     sleep 1
 done
